b27c888f7f7b85221512b51fb0212ce4d63bb2fa
Ediano Gama <eddy.tadim@gmail.com>
2021-08-22 19:06:49 -0300

    :new: login view

---
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/app/data/authentication/authentication_controller.dart';
+import 'package:flutter_app/app/data/authentication/authentication_state.dart';
+import 'package:flutter_app/app/modules/initial/views/initial_page.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+
+  final formKey = GlobalKey<FormState>();
+  final _authController = Get.find<AuthenticationController>();
+  // final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  final TextEditingController usernameController  = TextEditingController();
+  final TextEditingController passwordController  = TextEditingController();
+
+  void login() async  {
+    if(_authController.state is Authenticated){
+      Get.off(() => InitialPage());
+    }
+
+    if(usernameController.text.isEmpty) return null;
+
+    _authController.signIn(usernameController.text, passwordController.text);
+      
+  }
+    loginWithGoogle() async {
+      final LoginResult result = await FacebookAuth.i.login(
+        permissions: ['public_profile', 'email']
+      );
+      if (result.status == LoginStatus.success) {
+       final AccessToken accessToken = result.accessToken!;
+        await  _authController.signInWithData(accessToken);
+      }
+      print(result.status);
+    }
+
+}
---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/app/common/values/app_colors.dart';
+import 'package:flutter_app/app/common/values/strings.dart';
+import 'package:flutter_app/app/common/values/styles/dimens.dart';
+import 'package:flutter_app/app/modules/login/controllers/login_controller.dart';
+import 'package:flutter_app/app/modules/widgets/buttons/facebook_button.dart';
+import 'package:flutter_app/app/modules/widgets/buttons/text_button_gradient.dart';
+import 'package:flutter_app/app/modules/widgets/custom_inkwell_widget.dart';
+import 'package:flutter_app/app/modules/widgets/forms/input_field.dart';
+import 'package:flutter_app/app/modules/widgets/text/custom_column_text_widget.dart';
+import 'package:flutter_app/app/routes/app_pages.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+
+class LoginView extends GetView<LoginController> {
+  final controller = LoginController();
+  
+  Widget _formCompose() {
+    return Form(
+      key: controller.formKey,
+      child: Column(
+        children: <Widget>[
+          InputField(title: Strings.userName, controller: controller.usernameController, ),
+          InputField(title: Strings.password, isPassword: true, controller: controller.passwordController, )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+        body: Container(
+        height: height,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.h),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: height * .2),
+                    CustomColumnTextWidget(title: 'Login', subtitle: 'Log in to your account',),
+                    SizedBox(height: 50.h),
+                    _formCompose(),
+                    TextButtonGradient(title: Strings.logIn, onPressed: () => controller.login()),
+                    InkWell(
+                      onTap: () => {
+                        print('logar com facebook'),
+                        controller.loginWithGoogle()
+                      },
+                      child: FacebookButton(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CustomInkwellWidget(
+                            onTap: () {
+                                Get.toNamed(Routes.INITIAL);
+                              }, 
+                              child: Text(Strings.back,
+                                style: TextStyle(
+                                  color: AppColors.kPrimaryColor,
+                                    fontSize: Dimens.fontSize14, fontWeight: FontWeight.w500)
+                                ),
+                              )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ));
+  }
+}
